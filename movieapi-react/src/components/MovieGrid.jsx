4deb8072504c70dac4a3082dff5b263c46900a35
Lukaasss <lukas.schuler@student.htldornbirn.at>
2025-02-19 13:03:03 +0100

    Update MovieGrid.jsx

--- movieapi-react/src/components/MovieGrid.jsx
+++ movieapi-react/src/components/MovieGrid.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 
 const MovieGrid = ({ movies }) => {
     const navigate = useNavigate();
-    const displayedMovies = movies.slice(0, 10); // Nur 10 Filme anzeigen
+    const displayedMovies = movies.slice(0, 10);
 
     return (
         <div className="grid grid-cols-5 grid-rows-2 gap-6 p-8 w-full h-screen">
